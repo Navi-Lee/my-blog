2973facbbd2286b9924347264dbe6d41bbe90395
navi-lee <2625417961@qq.com>
2026-08-04 17:54:57 +0800

    improve site search metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,20 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Navi\u00A0Lee';
-export const SITE_DESCRIPTION = 'Welcome to my website! Github:Navi-lee ';
+export const SITE_URL = 'https://www.navilee.com';
+export const SITE_TITLE = 'Navi Lee';
+export const SITE_DESCRIPTION =
+	'Navi Lee 的个人博客，记录软件工程学习、技术实践、日常随笔与个人项目。';
+export const SITE_KEYWORDS = [
+	'Navi Lee',
+	'NaviLee',
+	'Navi Lee Blog',
+	'Navi Lee 个人博客',
+	'navilee.com',
+	'软件工程',
+	'技术博客',
+	'太原理工大学',
+];
+export const SITE_AUTHOR = 'Navi Lee';
+export const GITHUB_URL = 'https://github.com/Navi-Lee';
+export const CONTACT_EMAIL = 'LeeNavi@foxmail.com';
